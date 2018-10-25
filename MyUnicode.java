e2411733ba83dee3e489eb7d43b1770b68caa4ad
Porodin <Creshe2001@mail.ru>
2018-10-25 21:08:06 +0300

    corrected errors

--- MyUnicode.java
+++ MyUnicode.java
@@ -1,9 +1,8 @@
 public class MyUnicode {
 	public static void main (String[] args){
-		for(int i = 9398; i<10179 ;i++){
-			System.out.println(i);
+		for(int i = 33;i<=126;i++){
+			System.out.println((char) i + "  ");
 		}
 	}
 }
-	
 
