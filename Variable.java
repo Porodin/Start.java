e1862141d3f9f716e8852d66b9d709cb4d6cce52
Porodin <Creshe2001@mail.ru>
2018-11-14 12:17:21 +0300

    laptop specifications

---
+++ Variable.java
@@ -0,0 +1,21 @@
+public class Variable{
+	public static void main(String[] args){
+		byte v = 3; //обЪем кэша L3
+		short e = 2300; //частота процессора
+		int g = 2048; //обЪем видеопамяти 
+		long a = 5400; //скорость вращения жесткого диска
+		float f = 2.91f; //вес
+		double s = 15.6; //экран
+		char c = '5'; //процессор
+		boolean m = true;
+		System.out.println(v);
+		System.out.println(e);
+		System.out.println(g);
+		System.out.println(a);
+		System.out.println(f);
+		System.out.println(s);
+		System.out.println(c);
+		System.out.println(m);
+
+	}
+}
