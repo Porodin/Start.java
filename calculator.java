5fb72701414961385e306d5d18dee426cba14131
Porodin <Creshe2001@mail.ru>
2018-12-06 19:22:22 +0300

    New calculator

--- calculator.java
+++ calculator.java
@@ -1,30 +1,48 @@
 import java.util.Scanner;
 public class calculator {
-	public static void main(String[] args){
-	Scanner Сalculator = new Scanner (System.in);
-	int php, ruby, result;
-	System.out.println("Введите первое число:");
-	php = Сalculator.nextInt();
-	System.out.println("Введите второе число:");
-	ruby = Сalculator.nextInt();
-	result = php + ruby;
-	System.out.println("Сумма равна: + result");
-	result = ruby - php;
-	System.out.println("Разность равна: - result");
-	result = ruby * php;
-	System.out.println("Произведение равно: * result");
-	result = ruby / php;
-	System.out.println("Частное равно: / result");
-	result = ruby % php;
-	System.out.println("Остаток от деления: % result ");
+	public static void main(String[] args) {
 
- }
+		double num1 = new Scanner(System.in).nextDouble();
+		char operation = new Scanner(System.in).next().charAt(0);
+		double num2 = new Scanner(System.in).nextDouble();;
+		double result = 0;
+				if (operation == '+') {
+			result = num1 + num2;
+			System.out.println("The result of mathematical expression: " + result);
+		}
+		else if (operation == '-') {
+			result = num1 - num2;
+			System.out.println("The result of mathematical expression: " + result);
+		}
+		else if (operation == '*') {
+			result = num1 * num2;
+			System.out.println("The result of mathematical expression: " + result);
+		}
+		else if (operation == '/') {
+			result = num1 / num2;
+			System.out.println("The result of mathematical expression: " + result);
+		}
+		else if (operation == '^') {
+			result = num1;
+			for (int i = 1; i < num2; i++) {
+				result *= num1;
+			}
+			System.out.println("The result of mathematical expression: " + result);
+		}
+		else if (operation == '%') {
+			result = num1 % num2;
+			System.out.println("The result of mathematical expression: " + result);
+			}
+		}
+	}
 }
-
-
-
-
+				
 	
 	
 	
+
+
+		
+		
+		
 	
