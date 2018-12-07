d8a64ccd7ef425bd0778d59d2a7132a44dc64b90
Porodin <Creshe2001@mail.ru>
2018-12-07 11:00:09 +0300

    Fixed game

--- MyFirstGame.java
+++ MyFirstGame.java
@@ -1,21 +1,26 @@
-public class MyFirstGame {
-	public static void main(String [] args) {
-		Scanner scanner = new Scanner(System.in);
-		System.out.println("Задача угадать число.");
-		int range = 100;
-	    int number = (int) (Math.random() * range);
-	    while(true) {
-	    	System.out.println("Угадай число от 0 до " + range);
-	        int input_number = scanner.nextInt();
-	        if(input_number == number){
-	        	System.out.println("Да ты гений.");
-	        	break;
-	        } else if (input_number > number) {
-	        	System.out.println("Загаданное число меньше");
-	        } else {
-	        	System.out.println("Загаданное число больше");
-	        }
-            scanner.close();
-        }
-    }
-}
+import java.util.Scanner;
+import java.util.Random;
+public class MyFirstGame {	
+	public static void main (String[]args) {
+	int randomNumber;
+	int enteredNumber;
+	Random random = new Random();
+	Scanner scanner = new Scanner(System.in);
+
+	randomNumber = random.nextInt(100);
+
+	System.out.println("Enter number");
+	enteredNumber = scanner.nextInt();
+
+	if (enteredNumber > randomNumber){
+		System.out.println("The number you entered is greater than what the computer intered");
+	}
+	else if (enteredNumber < randomNumber){
+		System.out.println("The number you entered is less than what the computer");
+	}
+	else if (enteredNumber == randomNumber){
+	}
+	}
+	}
+
+
