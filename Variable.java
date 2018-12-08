2a3fe220a44876a4f84d833b07d6312451282f63
Porodin <Creshe2001@mail.ru>
2018-12-08 14:11:22 +0300

    Variable

--- Variable.java
+++ Variable.java
@@ -1,21 +1,21 @@
 public class Variable {
-	public static void main(String[] args){
-		byte l3 = 3;
-		short cpufrequency = 2300; 
-		int videomemorysize = 2048; 
-		long harddrivespeed = 5400; 
-		float weight = 2.91f; 
-		double screen = 15.6; 
-		char cpu = '5';
-		boolean randomaccessmemory = true;
+	public static void main(String[] args)
+{		byte l3 = 3;
+		short CpuFrequency = 2300; 
+		int VideoMemorySize = 2048; 
+		long HardDriveSpeed = 5400; 
+		float Weight = 2.91f; 
+		double Screen = 15.6; 
+		char Cpu = '5';
+		boolean RandomAccessMemory = true;
 		System.out.println(l3);
-		System.out.println(cpufrequency);
-		System.out.println(videomemorysize);
-		System.out.println(harddrivespeed);
-		System.out.println(weight);
-		System.out.println(screen);
-		System.out.println(cpu);
-		System.out.println("randomaccessmemory = 6");
+		System.out.println(CpuFrequency);
+		System.out.println(VideoMemorySize);
+		System.out.println(HardDriveSpeed);
+		System.out.println(Weight);
+		System.out.println(Screen);
+		System.out.println(Cpu);
+		System.out.println("RandomAccessMemory = 6");
 
 	}
 }
