a30b11b987f0d6b7f57aca0ae7235a97118bf1fd
Porodin <Creshe2001@mail.ru>
2018-12-14 10:21:54 +0300

    MyFirstGame

--- MyFirstGame.java
+++ MyFirstGame.java
@@ -2,11 +2,11 @@ public class MyFirstGame {
 	public static void main (String[]args) {
 		int userNumber = 30;
 		int computerNumber = 13;
-		while (userNumber != computerNumber) {
-			if (userNumber > computerNumber) {
+		while(userNumber != computerNumber) {
+			if(userNumber > computerNumber) {
 				System.out.println("Больше" + " " + userNumber);
 				userNumber--;
-			} else if (userNumber < computerNumber) {
+			} else if(userNumber < computerNumber) {
 				System.out.println(userNumber + " " + " Меньше");
 				userNumber++;
 			} else if(userNumber == computerNumber) {
