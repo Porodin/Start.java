e926be8db6857a2930940f0c12197998c1f0dd3e
Porodin <Creshe2001@mail.ru>
2018-12-19 12:05:06 +0300

    Calculator without Scanner

---
+++ Calculator.java
@@ -0,0 +1,31 @@
+public class Calculator {
+	public static void main(String[] args) {
+		int x = 10;
+		int y = 5;
+		char mathOperation = '^';
+
+		if(mathOperation == '+') {
+			System.out.println(x + y);
+		} else if(mathOperation == '-') {
+			System.out.println(x - y);
+		} else if(mathOperation == '/') {
+			System.out.println(x / y);
+		} else if(mathOperation == '*') {
+			System.out.println(x * y);
+		} else if(mathOperation == '%') {
+			System.out.println(x % y);
+		} else if(mathOperation == '^') {
+			int result = 1;
+			for(int i = 1; i <= y; i++) {
+			result = result*x;
+		}
+			System.out.println(x + y);
+			System.out.println(x - y);
+			System.out.println(x / y);
+			System.out.println(x * y);
+			System.out.println(x % y);
+			System.out.println(result);
+
+		}	
+	}	
+}
